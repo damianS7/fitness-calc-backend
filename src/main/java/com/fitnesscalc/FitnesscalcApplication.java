11249da578a297c1bc129ce7a80fc1ead54b1def
damianS7 <josepwnz@gmail.com>
2021-07-13 21:14:16 +0200

    estructura del proyecto

---
+++ src/main/java/com/fitnesscalc/FitnesscalcApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesscalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesscalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesscalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnesscalc/FitnesscalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesscalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesscalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
