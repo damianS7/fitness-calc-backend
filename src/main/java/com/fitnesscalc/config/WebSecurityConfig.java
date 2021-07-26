6ffebbcb8c1cf7945ed687f37ae0ef5a3706a2a9
damianS7 <>
2021-07-26 13:16:39 +0200

    Configuracion de seguridad y utilidades

--- src/main/java/com/fitnesscalc/config/WebSecurityConfig.java
+++ src/main/java/com/fitnesscalc/config/WebSecurityConfig.java
@@ -1,4 +1,88 @@
 package com.fitnesscalc.config;
 
-public class WebSecurityConfig {
+import com.fitnesscalc.auth.AuthorizationFilter;
+import com.fitnesscalc.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public WebSecurityConfig(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors()
+                .and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/users/login/**", "/api/users/registration/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().addFilter(new AuthorizationFilter(authenticationManager()));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        // Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager () throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
+
+    /*
+     * Debido a que el backend y el frontend corren en puertos diferentes
+     * por defecto Spring aplica CORS, el cual solo permite peticiones
+     * del mismo origen (esto incluye los puertos tambien) por lo que es necesario
+     * configurar CORS para permitir el puerto donde se ejecuta el frontend
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost"));
+        //configuration.setAllowedMethods(Arrays.asList("GET", "OPTIONS", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
