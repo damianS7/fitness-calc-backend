b6c43e9aabfebf99d1fe5fb69c83cd3755bb6d05
damianS7 <>
2021-07-26 13:20:35 +0200

    Food

---
+++ src/main/java/com/fitnesscalc/food/Food.java
@@ -0,0 +1,6 @@
+package com.fitnesscalc.food;
+
+public class Food {
+    private String name;
+    private int kcal;
+}
---
+++ src/main/java/com/fitnesscalc/food/FoodRepository.java
@@ -0,0 +1,4 @@
+package com.fitnesscalc.food;
+
+public class FoodRepository {
+}
